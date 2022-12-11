924f4cabf7e7a7734303a4b2571e2c4353049582
damirzecevic <damir_zecevic@hotmail.com>
2022-12-11 14:40:53 +0100

    Add the following test coverage:  1. Handling user's input and  2. Rendering an output based on input.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,19 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
   it('renders a heading, paragraph and an input', () => {
-    render(<Home />)
+    render(<Home />);
 
     const heading = screen.getByRole('heading', {
       level: 1,
-    })
+    });
 
     const paragraph = screen.getByTestId('roman-numeral-paragraph');
     const input = screen.getByTestId('user-number-input');
 
-    expect(heading).toBeInTheDocument()
-    expect(paragraph).toBeInTheDocument()
-    expect(input).toBeInTheDocument()
+    expect(heading).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
   })
+
+  it('changes output value shown in paragraph when input value changes', ()=>{
+    render(<Home />);
+
+    const paragraph = screen.getByTestId('roman-numeral-paragraph');
+    const input = screen.getByTestId('user-number-input');
+
+    fireEvent.change(input, {target: {value: 1}});
+    expect(paragraph).toHaveTextContent("I");
+  })
+
 })
