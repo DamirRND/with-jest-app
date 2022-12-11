a824456f9744072dfdbe0841e1df9d6853888db7
damirzecevic <damir_zecevic@hotmail.com>
2022-12-11 14:16:35 +0100

    Update test to check if page renders paragraph and an input.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,13 +2,18 @@ import { render, screen } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('renders a heading, paragraph and an input', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
+      level: 1,
     })
 
+    const paragraph = screen.getByTestId('roman-numeral-paragraph');
+    const input = screen.getByTestId('user-number-input');
+
     expect(heading).toBeInTheDocument()
+    expect(paragraph).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
   })
 })
