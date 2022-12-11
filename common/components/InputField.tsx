48864dee3dc40a9a4a6860865956550d2d1db533
damirzecevic <damir_zecevic@hotmail.com>
2022-12-11 23:33:25 +0100

    Change super-type of InputFieldProps from React.HtmlHTMLAttributes to React.InputHTMLAttributes.

--- common/components/InputField.tsx
+++ common/components/InputField.tsx
@@ -1,10 +1,6 @@
-import React, {ChangeEvent, FC} from "react";
+import React, {FC} from "react";
 
-interface InputFieldProps extends React.HtmlHTMLAttributes<HTMLInputElement> {
-    type: string;
-    value: number;
-    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
-}
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 export const InputField: FC<InputFieldProps> = ({type, value, onChange, ...rest}) => {
     return <input type={type} value={value} onChange={onChange} {...rest}/>;
