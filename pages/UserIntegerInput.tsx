0f315434d7670b7a99f53c3da1cac30a528c28dc
damirzecevic <damir_zecevic@hotmail.com>
2022-12-11 17:02:45 +0100

    Add <UserIntegerInput /> as custom component which implements InputField.tsx and custom function for handling the input.

---
+++ pages/UserIntegerInput.tsx
@@ -0,0 +1,18 @@
+import React, {ChangeEvent, FC, useCallback} from "react";
+import {InputField} from "../common/components/InputField";
+
+interface UserIntegerInputProps {
+    handleOnChange: (value: number) => void;
+    value: number;
+}
+
+export const UserIntegerInput: FC<UserIntegerInputProps> = ({handleOnChange, value}) => {
+    const handleChange = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => {
+            handleOnChange(event.target.valueAsNumber);
+        },
+        [handleOnChange]
+    );
+
+    return <InputField type="number" value={value} onChange={handleChange}/>
+}
